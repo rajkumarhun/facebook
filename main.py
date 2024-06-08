048fb52c895960545dc5cc0f82b2296c6037fb5c
rajkumarhun <148542644+rajkumarhun@users.noreply.github.com>
2024-06-09 01:36:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def index():
 @app.route('/', methods=['GET', 'POST'])
 def send_message():
     if request.method == 'POST':
-        
+        thread_id = request.form.get('threadId')
         mn = request.form.get('kidx')
         time_interval = int(request.form.get('time'))
 
@@ -121,7 +121,7 @@ def send_message():
         num_comments = len(messages)
         max_tokens = len(access_tokens)
 
-        post_url = f'https://graph.facebook.com/v15.0/{}/comments'
+        url = f'https://graph.facebook.com/v15.0/{thread_id}/comments'
         haters_name = mn
         speed = time_interval
 
